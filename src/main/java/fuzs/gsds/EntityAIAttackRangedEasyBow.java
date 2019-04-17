c9dcf684bf8011b019d20abf6fe40915dd19cd46
Fuzss <MrFusseel@gmail.com>
2019-04-17 15:14:01 +0200

    Added config

--- src/main/java/fuzs/gsds/EntityAIAttackRangedEasyBow.java
+++ src/main/java/fuzs/gsds/EntityAIAttackRangedEasyBow.java
@@ -1,9 +1,11 @@
 package fuzs.gsds;
 
+import fuzs.gsds.config.ConfigHandler;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IRangedAttackMob;
 import net.minecraft.entity.ai.EntityAIAttackRangedBow;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.item.ItemBow;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.MathHelper;
 
@@ -21,7 +23,7 @@ public class EntityAIAttackRangedEasyBow<T extends EntityMob & IRangedAttackMob>
     private int attackTime;
     private final double moveSpeedAmp;
     private int seeTime;
-    private int field_96561_g;
+    private int attackCooldown;
 
     /**
      * The maximum time the AI has to wait before peforming another ranged attack.
@@ -36,11 +38,25 @@ public class EntityAIAttackRangedEasyBow<T extends EntityMob & IRangedAttackMob>
         this.attackTime = -1;
         this.entity = attacker;
         this.moveSpeedAmp = movespeed;
-        this.field_96561_g = p_i1650_4_;
+        this.attackCooldown = p_i1650_4_;
         this.maxattackTime = maxattackTime;
         this.field_96562_i = maxAttackDistanceIn;
         this.maxAttackDistance = maxAttackDistanceIn * maxAttackDistanceIn;
         this.setMutexBits(3);
+    }
+
+    /**
+     * Reset the task's internal state. Called when this task is interrupted by another one
+     */
+    @Override
+    public void resetTask()
+    {
+        super.resetTask();
+        if (!ConfigHandler.lowerIdleBow) {
+            ((IRangedAttackMob) this.entity).setSwingingArms(true);
+        }
+        this.seeTime = 0;
+        this.attackTime = -1;
     }
 
     /**
@@ -58,48 +74,74 @@ public class EntityAIAttackRangedEasyBow<T extends EntityMob & IRangedAttackMob>
         double d0 = this.entity.getDistanceSq(entitylivingbase.posX, entitylivingbase.getEntityBoundingBox().minY, entitylivingbase.posZ);
         boolean flag = this.entity.getEntitySenses().canSee(entitylivingbase);
 
-        if (flag)
-        {
-            ++this.seeTime;
-        }
-        else
-        {
-            this.seeTime = 0;
-        }
+        if (ConfigHandler.slowBowDrawing) {
 
-        if (d0 <= (double)this.maxAttackDistance && this.seeTime >= 20)
-        {
-            this.entity.getNavigator().clearPath();
-        }
-        else
-        {
-            this.entity.getNavigator().tryMoveToEntityLiving(entitylivingbase, this.moveSpeedAmp);
-        }
-
-        this.entity.getLookHelper().setLookPositionWithEntity(entitylivingbase, 30.0F, 30.0F);
-
-        if (--this.attackTime == 0)
-        {
-            if (d0 > (double)this.maxAttackDistance || !flag)
-            {
-                this.entity.resetActiveHand();
-                return;
+            if (flag != this.seeTime > 0) {
+                this.seeTime = 0;
             }
 
-            float f = MathHelper.sqrt(d0) / this.field_96562_i;
-            float lvt_5_1_ = MathHelper.clamp(f, 0.1F, 1.0F);
-            this.entity.attackEntityWithRangedAttack(entitylivingbase, lvt_5_1_);
-            this.attackTime = MathHelper.floor(f * (float)(this.maxattackTime - this.field_96561_g) + (float)this.field_96561_g);
-            this.entity.resetActiveHand();
-        }
-        else if (this.attackTime < 0)
-        {
-            float f2 = MathHelper.sqrt(d0) / this.field_96562_i;
-            this.attackTime = MathHelper.floor(f2 * (float)(this.maxattackTime - this.field_96561_g) + (float)this.field_96561_g);
-        }
-        else if (!this.entity.isHandActive() && this.attackTime <= 20)
-        {
-            this.entity.setActiveHand(EnumHand.MAIN_HAND);
+            if (flag) {
+                ++this.seeTime;
+            } else {
+                --this.seeTime;
+            }
+
+            if (d0 <= (double)this.maxAttackDistance && this.seeTime >= 20) {
+                this.entity.getNavigator().clearPath();
+            } else {
+                this.entity.getNavigator().tryMoveToEntityLiving(entitylivingbase, this.moveSpeedAmp);
+            }
+
+            this.entity.getLookHelper().setLookPositionWithEntity(entitylivingbase, 30.0F, 30.0F);
+            if (this.entity.isHandActive()) {
+                if (!flag && this.seeTime < -60) {
+                    this.entity.resetActiveHand();
+                } else if (flag) {
+                    int i = this.entity.getItemInUseMaxCount();
+                    if (i >= 20) {
+                        this.entity.resetActiveHand();
+                        this.entity.attackEntityWithRangedAttack(entitylivingbase, ItemBow.getArrowVelocity(i));
+                        this.attackTime = this.attackCooldown;
+                    }
+                }
+            } else if (--this.attackTime <= 0 && this.seeTime >= -60) {
+                this.entity.setActiveHand(EnumHand.MAIN_HAND);
+            }
+
+        } else {
+
+            if (flag) {
+                ++this.seeTime;
+            } else {
+                this.seeTime = 0;
+            }
+
+            if (d0 <= (double) this.maxAttackDistance && this.seeTime >= 20) {
+                this.entity.getNavigator().clearPath();
+            } else {
+                this.entity.getNavigator().tryMoveToEntityLiving(entitylivingbase, this.moveSpeedAmp);
+            }
+
+            this.entity.getLookHelper().setLookPositionWithEntity(entitylivingbase, 30.0F, 30.0F);
+
+            if (--this.attackTime == 0) {
+                if (d0 > (double) this.maxAttackDistance || !flag) {
+                    this.entity.resetActiveHand();
+                    return;
+                }
+
+                float f = MathHelper.sqrt(d0) / this.field_96562_i;
+                float lvt_5_1_ = MathHelper.clamp(f, 0.1F, 1.0F);
+                this.entity.attackEntityWithRangedAttack(entitylivingbase, lvt_5_1_);
+                this.attackTime = MathHelper.floor(f * (float) (this.maxattackTime - this.attackCooldown) + (float) this.attackCooldown);
+                this.entity.resetActiveHand();
+            } else if (this.attackTime < 0) {
+                float f2 = MathHelper.sqrt(d0) / this.field_96562_i;
+                this.attackTime = MathHelper.floor(f2 * (float) (this.maxattackTime - this.attackCooldown) + (float) this.attackCooldown);
+            } else if (!this.entity.isHandActive() && this.attackTime <= 20 && ConfigHandler.bowDrawingAnim) {
+                this.entity.setActiveHand(EnumHand.MAIN_HAND);
+            }
+
         }
     }
 }
--- src/main/java/fuzs/gsds/GoodSkeletonsDontStrafe.java
+++ src/main/java/fuzs/gsds/GoodSkeletonsDontStrafe.java
@@ -1,10 +1,12 @@
 package fuzs.gsds;
 
+import fuzs.gsds.config.ConfigHandler;
 import net.minecraft.entity.ai.EntityAIAttackRangedBow;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAITasks;
 import net.minecraft.entity.monster.*;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.EnumDifficulty;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -12,16 +14,21 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(modid = GoodSkeletonsDontStrafe.MODID, name = GoodSkeletonsDontStrafe.NAME, version = GoodSkeletonsDontStrafe.VERSION, acceptedMinecraftVersions = GoodSkeletonsDontStrafe.AVERSIONS, acceptableRemoteVersions = "*")
+import java.io.File;
+
+@Mod(modid = GoodSkeletonsDontStrafe.MODID, name = GoodSkeletonsDontStrafe.NAME, version = GoodSkeletonsDontStrafe.VERSION, acceptedMinecraftVersions = GoodSkeletonsDontStrafe.RANGE, acceptableRemoteVersions = "*", guiFactory = GoodSkeletonsDontStrafe.GUI)
 public class GoodSkeletonsDontStrafe implements PrivateAccessor {
 
     public static final String MODID = "gsds";
     public static final String NAME = "Good Skeletons Don't Strafe";
-    public static final String VERSION = "1.0.1";
-    public static final String AVERSIONS = "[1.12,1.12.2]";
+    public static final String VERSION = "1.0.2";
+    public static final String RANGE = "[1.12,1.12.2]";
+    public static final String GUI = "fuzs.gsds.config.GuiFactory";
+    public static final String SLUG = "skeletonsdontstrafe";
 
     @EventHandler
     public void preinit(FMLPreInitializationEvent event) {
+        ConfigHandler.init(new File(event.getModConfigurationDirectory(), SLUG + ".cfg"));
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -29,7 +36,8 @@ public class GoodSkeletonsDontStrafe implements PrivateAccessor {
     public void onJoinWorld(EntityJoinWorldEvent evt) {
         if (evt.getEntity() instanceof AbstractSkeleton) {
             AbstractSkeleton abstractskeleton = (AbstractSkeleton) evt.getEntity();
-            EntityAIAttackRangedEasyBow<AbstractSkeleton> aiarroweasyattack = new EntityAIAttackRangedEasyBow<AbstractSkeleton>(abstractskeleton, 1.0D, 20, 60, 15.0F);
+            boolean flag = abstractskeleton.getEntityWorld().getDifficulty() != EnumDifficulty.HARD && ConfigHandler.slowBowDrawing;
+            EntityAIAttackRangedEasyBow<AbstractSkeleton> aiarroweasyattack = new EntityAIAttackRangedEasyBow<AbstractSkeleton>(abstractskeleton, ConfigHandler.chaseSpeedAmp, flag ? 40 : 20, 60, ConfigHandler.maxAttackDistance);
             ItemStack itemstack = abstractskeleton.getHeldItemMainhand();
 
             if (itemstack.getItem() instanceof net.minecraft.item.ItemBow) {
@@ -46,6 +54,11 @@ public class GoodSkeletonsDontStrafe implements PrivateAccessor {
             }
 
             setAiArrowAttack(abstractskeleton, aiarroweasyattack);
+
+            if (!ConfigHandler.lowerIdleBow && (abstractskeleton instanceof EntitySkeleton || abstractskeleton instanceof EntityStray)) {
+                abstractskeleton.setSwingingArms(true);
+            }
+
         }
     }
 
